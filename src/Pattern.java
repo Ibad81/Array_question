8765cf19ecd8c362e560625b2eee5e7d67a6e68d
ibad81 <ibadbaig921@gmail.com>
2022-01-13 21:11:52 +0530

    second commit

--- src/Pattern.java
+++ src/Pattern.java
@@ -1,15 +1,15 @@
-//import java.util.Scanner;
-//
-////public class Pattern {
-////    public static void main(String[] args) {
-////        Scanner sc=new Scanner(System.in);
-//        int n=sc.nextInt();
-//        for (int i = 0; i <=n; i++) {
-//            System.out.println("");
-//            for (int j = 0; j < i ; j++) {
-//                System.out.print("*");
-////                System.out.println("\n);
-//            }
-//        }
-//    }
-//}
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 0; i <=n; i++) {
+            System.out.println("");
+            for (int j = 0; j < i ; j++) {
+                System.out.print("*");
+//                System.out.println("\n);
+            }
+        }
+    }
+}
